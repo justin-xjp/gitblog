56a8405a5f4429f8a6463bafc1349915e434d37c
justin-xjp <34906921+justin-xjp@users.noreply.github.com>
2023-11-22 14:30:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from lxml.etree import CDATA
 from marko.ext.gfm import gfm as marko
 
 MD_HEAD = """## Gitblog
-My personal blog using issues and GitHub Actions (随意转载，无需署名)
+My personal blog using issues and GitHub Actions 
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
 
